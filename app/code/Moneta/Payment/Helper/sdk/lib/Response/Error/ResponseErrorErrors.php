19ba27c6481f6e81df932a4e5abf84149b784180
Learyeoin <eoin.oleary@evopayments.com>
2021-09-16 12:33:02 +0100

    ISO Update

--- app/code/Moneta/Payment/Helper/sdk/lib/Response/Error/ResponseErrorErrors.php
+++ app/code/Moneta/Payment/Helper/sdk/lib/Response/Error/ResponseErrorErrors.php
@@ -6,7 +6,6 @@ use Moneta\Payment\Helper\sdk\lib\Response;
 
 class ResponseErrorErrors extends Response {
 
-
     public function __construct($errors = array())
         {
             if (is_array($errors)) {
@@ -21,16 +20,5 @@ class ResponseErrorErrors extends Response {
                 $this->_data['errors'] = $errors;
             }
         }
-
-    public function __construct($errors = array()) {
-        if (is_array($errors)) {
-            foreach ($errors as $error) {
-                $this->_data[$error] = $error;
-            }
-        } else {
-            $this->_data[$errors] = $errors;
-        }
-    }
-
 
 }
